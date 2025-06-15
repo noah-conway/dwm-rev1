d6ef6a5cfaba097d3de4d71f7856022190fc0314
noah-conway <nconway@posteo.net>
2025-06-15 09:57:59 -0700

    Adding keybinds from previous revision

--- config.h
+++ config.h
@@ -9,7 +9,7 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int horizpadbar        = 2;        /* horizontal padding for statusbar */
 static const int vertpadbar         = 0;        /* vertical padding for statusbar */
-static const char *fonts[]          = { "monospace:size=10" };
+static const char *fonts[]          = { "Symbols Nerd Font Mono:size=11:style=Regular", "Roberto Mono:size=11:style=Italic"};
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
@@ -37,7 +37,9 @@ static Sp scratchpads[] = {
 };
 
 /* tagging */
-static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+static const char *tags[] = { "󰽧", "󰽡", "󰽨", "", "󰽦", "󰽣", "󰽥"};
+static const char *tagsalt[] = { "a", "s", "d", "f", "w", "e", "g"};
+static const char *tagsnum[] = { "1", "2", "3", "4", "5", "6", "7"};
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -84,6 +86,20 @@ static const Layout layouts[] = {
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, NULL };
 static const char *termcmd[]  = { "st", NULL };
+static const char *browsercmd[] = { "firefox", NULL};
+static const char *pbrowsercmd[] = { "firefox", "-private-window", NULL};
+static const char *btcmd[] = { "dmenu-bluetooth", NULL};
+static const char *mailcmd[] = { "thunderbird", NULL};
+static const char *scrotcmd[] = { "flameshot", "gui", NULL};
+static const char *powercmd[] = { "powermenu.sh", NULL};
+
+static const char *mpdtogglecmd[] = { "mpc", "toggle", NULL};
+static const char *mpdnextcmd[] = { "mpc", "next", NULL};
+static const char *mpdprevcmd[] = { "mpc", "prev", NULL};
+static const char *volraisecmd[] = { "pamixer", "-i", "10", NULL};
+static const char *vollowercmd[] = { "pamixer", "-d", "10", NULL};
+static const char *volmutecmd[] = { "pamixer", "--toggle-mute", NULL};
+static const char *audioswtchcmd[] = { "audioswitch.sh", NULL};
 
 static const Arg tagexec[] = {
 	{ .v = termcmd },
@@ -98,21 +114,33 @@ static const Arg tagexec[] = {
 };
 
 static const Key keys[] = {
+  /* ### LAUNCH PROGRAMS ### */
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
+	{ MODKEY,                       XK_Return, spawn,          {.v = termcmd } },
+	{ MODKEY,                       XK_b,      spawn,          {.v = browsercmd } },
+  /* ### MEDIA KEYS ### */
+	/* modifier                     key        function        argument */
+  { 0,                            XF86XK_AudioPlay,             spawn,          {.v = mpdtogglecmd } },
+	{ 0,                            XF86XK_AudioNext,             spawn,          {.v = mpdnextcmd } },
+	{ 0,                            XF86XK_AudioPrev,             spawn,          {.v = mpdprevcmd } },
+	{ 0,                            XF86XK_AudioRaiseVolume,      spawn,          {.v = volraisecmd } },
+	{ 0,                            XF86XK_AudioLowerVolume,      spawn,          {.v = vollowercmd } },
+	{ 0,                            XF86XK_AudioMute,             spawn,          {.v = volmutecmd } },
+  /* ### WM Keys ### */
+	/* modifier                     key        function        argument */
+	//{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+	{ MODKEY,                       XK_equal,      incnmaster,     {.i = +1 } },
+	{ MODKEY,                       XK_minus,      incnmaster,     {.i = -1 } },
 	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
 	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+	{ MODKEY,                       XK_Tab,    zoom,           {0} },
+	{ MODKEY|ShiftMask,             XK_Tab,    view,           {0} },
+	{ MODKEY,                       XK_q,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
-	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
+	{ MODKEY,                       XK_r,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
@@ -122,22 +150,20 @@ static const Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
-	{ MODKEY,            			XK_y,  	   togglescratch,  {.ui = 0 } },
-	{ MODKEY,            			XK_u,	   togglescratch,  {.ui = 1 } },
-	{ MODKEY,            			XK_x,	   togglescratch,  {.ui = 2 } },
-	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
-	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
-	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
-	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
-	TAGKEYS(                        XK_1,                      0)
-	TAGKEYS(                        XK_2,                      1)
-	TAGKEYS(                        XK_3,                      2)
-	TAGKEYS(                        XK_4,                      3)
-	TAGKEYS(                        XK_5,                      4)
-	TAGKEYS(                        XK_6,                      5)
-	TAGKEYS(                        XK_7,                      6)
-	TAGKEYS(                        XK_8,                      7)
-	TAGKEYS(                        XK_9,                      8)
+	{ MODKEY,            			      XK_y,  	   togglescratch,  {.ui = 0 } },
+	{ MODKEY,            			      XK_p,	     togglescratch,  {.ui = 1 } },
+	{ MODKEY,            			      XK_o,	     togglescratch,  {.ui = 2 } },
+	{ MODKEY,                       XK_lefbracket,  setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_rightbracket,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_leftbracket,  setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_rightbracket,  setgaps,        {.i = GAP_TOGGLE} },
+	TAGKEYS(                        XK_a,                      0)
+	TAGKEYS(                        XK_s,                      1)
+	TAGKEYS(                        XK_d,                      2)
+	TAGKEYS(                        XK_f,                      3)
+	TAGKEYS(                        XK_w,                      4)
+	TAGKEYS(                        XK_e,                      5)
+	TAGKEYS(                        XK_g,                      6)
 	{ MODKEY|ShiftMask,             XK_q,      quit,           {0} },
 };
 
